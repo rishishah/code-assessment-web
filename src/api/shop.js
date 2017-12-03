c101010c292beff641cd7565c22b6ffd3aef3aad
Rishi Shah <shah.rishi05@gmail.com>
2017-12-03 15:47:25 -0500

    Update to fetch and use product data from endpoint

--- src/api/shop.js
+++ src/api/shop.js
@@ -4,8 +4,18 @@
 import _products from './products.json'
 
 const TIMEOUT = 100
+const PRODUCT_ENDPOINT = 'http://tech.work.co/shopping-cart/products.json'
+
+const mapToProductModel = products =>
+  products.map(product => {
+    const { id, productTitle, price, inventory } = product;
+    return { id, title: productTitle, price: price.value, inventory };
+  });
 
 export default {
-  getProducts: (cb, timeout) => setTimeout(() => cb(_products), timeout || TIMEOUT),
+  getProducts: cb =>
+    fetch(PRODUCT_ENDPOINT)
+      .then(response => response.json())
+      .then(data => cb(mapToProductModel(data))),
   buyProducts: (payload, cb, timeout) => setTimeout(() => cb(), timeout || TIMEOUT)
 }
